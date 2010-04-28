ff648ce608ca8134ba168107138979bab7a9b044
martin.h.pedersen <martin.h.pedersen@74d166aa-0315-11df-b7b4-3d6a74a08a84>
2010-04-28 13:13:06 +0000

    View for drawing our logo.

---
+++ trunk/HaikuTwitter/HTGLogoView.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010 Martin Hebnes Pedersen, martinhpedersen @ "google mail"
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */ 
+
+#include "HTGLogoView.h"
+
+HTGLogoView::HTGLogoView()
+	: BView("logo", B_WILL_DRAW)
+{
+	fLogo = BTranslationUtils::GetBitmap(B_VECTOR_ICON_TYPE, "twitter_icon");
+}
+
+
+HTGLogoView::~HTGLogoView()
+{
+	delete fLogo;
+}
+
+void HTGLogoView::AttachedToWindow()
+{
+   if ( Parent() )
+      SetViewColor(Parent()->ViewColor());
+   BView::AttachedToWindow();
+}
+
+BSize
+HTGLogoView::MinSize()
+{
+	if (fLogo == NULL)
+		return BSize(200, 200);
+
+	return BSize(fLogo->Bounds().Width(), fLogo->Bounds().Height());
+}
+
+
+BSize
+HTGLogoView::MaxSize()
+{
+	if (fLogo == NULL)
+		return BSize(300, 300);
+
+	return BSize(B_SIZE_UNLIMITED, fLogo->Bounds().Height());
+}
+
+
+void
+HTGLogoView::Draw(BRect updateRect)
+{
+	if (fLogo != NULL) {
+		SetDrawingMode(B_OP_ALPHA);
+		DrawBitmap(fLogo,
+			BPoint((Bounds().Width() - fLogo->Bounds().Width()) / 2, 0));
+		SetDrawingMode(B_OP_OVER);
+	}
+}
