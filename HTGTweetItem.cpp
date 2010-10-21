d09bf6bf825cbc7a87ce58c6f610c37451e2b78b
martin.h.pedersen <martin.h.pedersen@gmail.com>
2010-10-21 01:26:53 +0000

    Fixed a memory leak

--- HTGTweetItem.cpp
+++ HTGTweetItem.cpp
@@ -123,7 +123,6 @@ HTGTweetItem::DrawItem(BView *owner, BRect frame, bool complete)
 	
 	/*Draw userIcon*/
 	if(!theTweet->isDownloadingBitmap()) {
-		//theTweet->waitUntilDownloadComplete();
 		owner->SetDrawingMode(B_OP_ALPHA);
 		owner->DrawBitmapAsync(theTweet->getBitmap(), BRect(frame.left+9, frame.top+5+((Height()-60)/2), frame.left+48+8, frame.top+72-20+((Height()-60)/2)));
 		owner->SetDrawingMode(B_OP_OVER);
