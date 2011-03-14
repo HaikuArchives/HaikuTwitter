04bb9e01f9cbca9676ee1abb8aa44e4d4000bfc8
martin.h.pedersen <martin.h.pedersen@gmail.com>
2011-03-14 15:06:02 +0000

    More work on Issue 12 (Branding).

---
+++ HTGStatusBar.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2010-2011 Martin Hebnes Pedersen, martinhpedersen @ "google mail"
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */ 
+
+#include <View.h>
+#include <TranslationUtils.h>
+#include <Roster.h>
+#include <Bitmap.h>
+#include <iostream>
+
+#ifndef HTG_STATUSBAR_H
+#define HTG_STATUSBAR_H
+
+class HTGStatusBar : public BView {
+public:
+							HTGStatusBar(BRect frame);
+	virtual					~HTGStatusBar();
+
+	virtual void			Draw(BRect updateRect);
+	virtual void			AttachedToWindow();
+			void			SetStatus(const char*);
+private:
+			const char*		status;
+};
+#endif
